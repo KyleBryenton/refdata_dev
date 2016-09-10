b23dd6106fe911175a1c9b73c864c345a724e97c
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-09-10 14:23:21 -0700

    added shield's water cluster set

--- 40_eval/eval_driver.m
+++ 40_eval/eval_driver.m
@@ -16,16 +16,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Choose the energy reader and name generator
-source("reader_g09.m");
-# source("reader_postg.m");
+# source("reader_g09.m");
+source("reader_postg.m");
 # source("reader_orca.m");
 # source("reader_postg_psi4.m");
  
 data = {...
-        "/home/alberto/git/refdata/30_run/20_p26",...
+        "/home/alberto/git/refdata/30_run/20_water",...
       };
 din = {...
-        "../10_din/p26.din",...
+        "../10_din/water.din",...
       };
 
 #### Now DO stuff ####
