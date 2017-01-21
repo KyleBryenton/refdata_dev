ac2f1908ee098557589e3b74df021fb8a50905c8
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2017-01-21 11:04:06 -0800

    clean up the 40_eval routines

--- 40_eval/eval_driver.m
+++ 40_eval/eval_driver.m
@@ -16,12 +16,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Choose the energy reader and name generator
-# source("reader_g09.m");
-# source("reader_postg.m");
-# source("reader_d3.m");
-source("reader_espresso.m");
-# source("reader_orca.m");
-# source("reader_postg_psi4.m");
+# source("reader_g09-simple.m");
+# source("reader_postg-simple.m");
+# source("reader_d3-simple.m");
+source("reader_espresso-simple.m");
+# source("reader_orca-simple.m");
+# source("reader_postg_psi4-simple.m");
  
 data = {...
         "/home/alberto/calc/xdm_opt/15_zerop/b86bpbe-xdm",...
@@ -70,4 +70,3 @@ for i = 1:ndin
   ## printf("| %s | %s | %.3f |\n",data{i},din{i},mad(1));
 
 endfor
-
